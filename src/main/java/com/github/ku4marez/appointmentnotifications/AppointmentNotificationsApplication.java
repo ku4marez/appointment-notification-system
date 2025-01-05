ba172237b3db2ebb8618a30e81e4ed5978325960
Anton Kucha <anton.kucha@aionys.com>
2025-01-05 18:56:43 +0100

    Init project

---
+++ src/main/java/com/github/ku4marez/appointmentnotifications/AppointmentNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.github.ku4marez.appointmentnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ku4marez/appointmentnotifications/AppointmentNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ku4marez.appointmentnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
