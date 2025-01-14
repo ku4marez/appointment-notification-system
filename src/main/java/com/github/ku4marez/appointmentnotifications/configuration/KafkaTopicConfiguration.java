1d8d85c2c9c93bd4f2eb87fd6f9e0a7f920c6274
Anton Kucha <anton.kucha@aionys.com>
2025-01-15 00:38:28 +0100

    Small fix

--- src/main/java/com/github/ku4marez/appointmentnotifications/configuration/KafkaTopicConfiguration.java
+++ src/main/java/com/github/ku4marez/appointmentnotifications/configuration/KafkaTopicConfiguration.java
@@ -1,13 +1,17 @@
 package com.github.ku4marez.appointmentnotifications.configuration;
 
-import com.github.ku4marez.commonlibraries.constant.KafkaConstants;
 import com.github.ku4marez.commonlibraries.util.KafkaTopicUtil;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 
 import java.util.List;
+
+import static com.github.ku4marez.commonlibraries.constant.KafkaConstants.APPOINTMENT_CREATED_TOPIC;
+import static com.github.ku4marez.commonlibraries.constant.KafkaConstants.APPOINTMENT_UPDATED_TOPIC;
 
 @Configuration
 public class KafkaTopicConfiguration {
@@ -20,12 +24,12 @@ public class KafkaTopicConfiguration {
         return new KafkaTopicUtil(bootstrapServers);
     }
 
-    @Bean
-    public void setupTopics(KafkaTopicUtil kafkaTopicManager) {
+    @EventListener(ApplicationReadyEvent.class)
+    public void setupTopics() {
+        KafkaTopicUtil kafkaTopicManager = kafkaTopicManager();
         List<NewTopic> topics = List.of(
-                new NewTopic(KafkaConstants.APPOINTMENT_CREATED_TOPIC, 3, (short) 1),
-                new NewTopic(KafkaConstants.APPOINTMENT_UPDATED_TOPIC, 3, (short) 1),
-                new NewTopic(KafkaConstants.NOTIFICATION_SENT_TOPIC, 3, (short) 1)
+                new NewTopic(APPOINTMENT_CREATED_TOPIC, 3, (short) 1),
+                new NewTopic(APPOINTMENT_UPDATED_TOPIC, 3, (short) 1)
         );
 
         kafkaTopicManager.createTopics(topics);
